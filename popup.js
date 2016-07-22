fc3be7bc4a046b5d6e3ad3e19ade5a3c8ae3ba18
k24dizzle <zhaok24@uw.edu>
2016-07-22 10:31:14 -0700

    it works?

--- popup.js
+++ popup.js
@@ -1,4 +1,13 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     alert('nick has no knees');
+    console.log($("#count").html())
+    // whenever the jtb button is clicked, it executes this function
+    $("#jtb").click(function(e) {
+        // prevents default behavior of the button
+        e.preventDefault();
+        // takes the current "count" value and turns it into an int
+        var current = parseInt($("#count").html());
+        // sets the count to incrase by 1
+        $("#count").html(current + 1);
+    });
 });
