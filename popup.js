c41d9678498f473a0786fe724f9a817582680cef
k24dizzle <zhaok24@uw.edu>
2016-08-15 10:01:36 -0700

    added reset button

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert('nick has no knees');
+    // alert('nick has no knees');
+    
+    // on load, if there's a value in the storage, load it up
     chrome.storage.sync.get('count', function(result) {
         console.log(result.count);
+        // if there is a value
         if (result.count != null) {
             $("#count").html(result.count);
         }
-
     });
     
     // whenever the jtb button is clicked, it executes this function
@@ -15,13 +17,22 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         // takes the current "count" value and turns it into an int
         var current = parseInt(count.html());
-        // sets the count to incrase by 1
+        // sets the count to increase by 1
         count.html(current + 1);
-
+        // save value in storage under key "count"
         chrome.storage.sync.set({'count':  count.html()}, function() {
-
             console.log('Settings saved: ' + count.html());
-        
         });
+    });
+
+    // whenever the reset button is clicked, executes this function
+    $("#reset").click(function(e) {
+        // set the count back to 0
+        $("#count").html(0);
+        chrome.storage.sync.set({'count':  count.html()}, function() {
+            console.log('Reset Count');        
+        });
+
+
     });
 });
